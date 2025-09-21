ed83c9efac0a93c7545507e19ff6be6ee7a51fca
Nicole <2521002s@student.gla.ac.uk>
2025-09-21 20:02:31 +0100

    Toggle Theme Button

--- src/GUI.java
+++ src/GUI.java
@@ -3,18 +3,44 @@ import com.formdev.flatlaf.*;
 
 public class GUI {
     JFrame frame;
+    JButton themeButton;
+    JPanel themePanel;
+    boolean isDark;
 
     GUI() {
-        FlatDarkLaf.setup();
-        frame = new JFrame("Sport Broadcast");
+        initialise();
         show();
     }
 
-    void show() {
+    private void initialise() {
+        FlatLightLaf.setup();
+        isDark = false;
+        frame = new JFrame("Sport Broadcast");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        themeButton = new JButton("Toggle Theme");
+        themeButton.setFocusable(false);
+        themeButton.addActionListener(e -> toggleTheme(isDark));
+        themePanel = new JPanel();
+        themePanel.add(themeButton);
+        frame.add(themePanel);
     }
 
+    private void show() {
+        frame.setVisible(true);
+    } 
+
+    private void toggleTheme(boolean isDark) {
+        if (isDark) {
+            FlatLightLaf.setup();
+            this.isDark = false;
+        }
+        else {
+            FlatDarkLaf.setup();
+            this.isDark = true;
+        }
+        FlatLaf.updateUI();
+    }
+ 
     
 }
